b8b8657128fe57321ef3d052a5f10ee04a69ead3
Druv21 <dhruvag911@gmail.com>
2022-04-17 01:46:17 +0530

    More Updates

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -2,7 +2,54 @@ import React from 'react'
 
 const Hello = () => {
   return (
-    <h1>Hello</h1>
+    <div>
+      <form className="login">
+                    <div className="login__field">
+                        <i className="login__icon fas fa-user"></i>
+                        <input type="text" className="login__input" placeholder="Enter Team Leader Name"/>
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-lock"></i>
+                        <input type="number" className="login__input" placeholder="Enter Student Number"/>
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-lock"></i>
+                        <input type="number" className="login__input" placeholder="Enter Mobile Number"/>
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-user"></i>
+                        <input type="text" className="login__input" placeholder="Enter Section"/>
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-user"></i>
+                        <input type="text" className="login__input" placeholder="Enter Member 1 Name"/> 
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-lock"></i>
+                        <input type="number" className="login__input" placeholder="Enter Student Number"/>
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-user"></i>
+                        <input type="text" className="login__input" placeholder="Enter Member 2 Name"/> 
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-lock"></i>
+                        <input type="number" className="login__input" placeholder="Enter Student Number"/>
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-user"></i>
+                        <input type="text" className="login__input" placeholder="Enter Member 3 Name"/> 
+                    </div>
+                    <div className="login__field">
+                        <i className="login__icon fas fa-lock"></i>
+                        <input type="number" className="login__input" placeholder="Enter Student Number"/>
+                    </div>
+                    <button className="button login__submit">
+                        <span className="button__text">Submit</span>
+                        <i className="button__icon fas fa-chevron-right"></i>
+                    </button>			
+                </form>
+    </div>
   )
 }
 
