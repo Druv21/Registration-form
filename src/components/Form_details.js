1809b13fd12287641d11d85b1259052fc7af2a06
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2022-05-02 01:57:28 +0530

    OTP verified, resend otp route added and autofill email

--- src/components/Form_details.js
+++ src/components/Form_details.js
@@ -1,66 +1,152 @@
-import React from 'react';
-import '../css/reg_form.css';
-import blacklogo from '../assets/black.png';
+import React from "react";
+import "../css/reg_form.css";
+import blacklogo from "../assets/black.png";
+import { useState, useEffect } from "react";
 
 const Form2 = () => {
+  const email = localStorage.getItem("email");
+  const [details, storeDetails] = useState([]);
+  var detail = "";
+  const store_details = (event) => {
+    storeDetails(event.target.value);
+  };
+  const print = (e) => {
+    e.preventDefault();
+    console.log(detail);
+  };
 
   return (
-    <div className="container">     
+    <div className="container">
       <div className="screen">
         <div className="screen__content">
           <div>
-            <img src={blacklogo} alt="Logo" height="100" width="120"/>
+            <img src={blacklogo} alt="Logo" height="100" width="120" />
           </div>
           <form className="login form2">
             <div className="scroll">
-             <div className="login__field">          
-                <input type="text" className="login__input" placeholder="Enter Team Leader Name" required/>
-             </div>
-             <div className="login__field">
-                <input type="email" className="login__input" placeholder="Enter Email" required/>
-             </div>
-             <div className="login__field">      
-                <input type="text" className="login__input" placeholder="Enter Student Number" required/>
-             </div>
-             <div className="login__field">          
-                <input type="text" className="login__input" placeholder="Enter Mobile Number" maxLength="10" required/>
-             </div>
-             <div className="login__field">
-                <input type="text" className="login__input" placeholder="Enter Section" required/>
-             </div>
-             <div className="login__field form3">
-                <input type="text" className="login__input" placeholder="Enter Member 1 Name" required/>
-             </div>
-             <div className="login__field form3">
-                <input type="text" className="login__input" placeholder="Enter Student Number" required/>
-             </div>
-             <div className="login__field form3">
-                <input type="text" className="login__input" placeholder="Enter Member 2 Name" required/>
-             </div>
-             <div className="login__field form3">
-                <input type="text" className="login__input" placeholder="Enter Student Number" required/>
-             </div>
-             <div className="login__field form3">
-                <input type="text" className="login__input" placeholder="Enter Member 3 Name" required/>
-             </div>
-             <div className="login__field form3">
-                <input type="text" className="login__input" placeholder="Enter Student Number" required/>
-             </div>
+              <div className="login__field">
+                <input
+                  type="text"
+                  name="field-1"
+                  className="login__input"
+                  placeholder="Enter Team Leader Name"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field">
+                <input
+                  type="email"
+                  className="login__input"
+                  value={email}
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field">
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="Enter Student Number"
+                  maxLength="7"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field">
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="Enter Mobile Number"
+                  maxLength="10"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field">
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="Enter Section"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field form3">
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="Enter Member 1 Name"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field form3">
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="Enter Student Number"
+                  maxLength="7"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field form3">
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="Enter Member 2 Name"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field form3">
+                <input
+                  type="text"
+                  name="field-4"
+                  className="login__input"
+                  placeholder="Enter Student Number"
+                  maxLength="7"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field form3">
+                <input
+                  type="text"
+                  name="field-3"
+                  className="login__input"
+                  placeholder="Enter Member 3 Name"
+                  required
+                  onChange={store_details}
+                />
+              </div>
+              <div className="login__field form3">
+                <input
+                  type="text"
+                  name="field-4"
+                  className="login__input"
+                  placeholder="Enter Student Number"
+                  maxLength="7"
+                  required
+                  onChange={store_details}
+                />
+              </div>
             </div>
-            <button className="login__submit" type="button">
-                <span className="button__text button3">Submit</span>       
+            <button className="login__submit" onClick={print}>
+              <span className="button__text button3">Submit</span>
             </button>
-          </form>      
+          </form>
         </div>
         <div className="screen__background">
-            <span className="screen__background__shape screen__background__shape4"></span>
-            <span className="screen__background__shape screen__background__shape3"></span>
-            <span className="screen__background__shape screen__background__shape2"></span>
-            <span className="screen__background__shape screen__background__shape1"></span>
+          <span className="screen__background__shape screen__background__shape4"></span>
+          <span className="screen__background__shape screen__background__shape3"></span>
+          <span className="screen__background__shape screen__background__shape2"></span>
+          <span className="screen__background__shape screen__background__shape1"></span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Form2;
--- src/components/Otp_validation.js
+++ src/components/Otp_validation.js
@@ -1,136 +1,200 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import '../css/reg_form.css';
-import {useState} from 'react';
-import blacklogo from '../assets/black.png';
-import axios from 'axios';
-
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "../css/reg_form.css";
+import { useState } from "react";
+import blacklogo from "../assets/black.png";
+import axios from "axios";
 
 function Form() {
+  var otp = "";
+  var nextfield = "";
+  var count = 0;
 
-    var otp="";
-    var nextfield="";
-    var count=0;
+  const history = useHistory();
+  const [show, setShow] = useState(false);
+  const [email, setemail] = useState("");
 
-       const history=useHistory();
-       const [show, setShow] = useState(false);
-       const [email,setemail]=useState("");
+  const postapi = async () => {
+    axios
+      .post("https://eventformconatus.herokuapp.com/sendotp", { email: email })
+      .then(function (response) {
+        console.log(response);
+        setShow(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-       const postapi=async()=>{
-        axios.post("https://eventformconatus.herokuapp.com/sendotp",{"email":email})
-        .then(function (response) {
-          console.log(response);
-          setShow(true);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+  const showdiv = (e) => {
+    e.preventDefault();
+    setShow(true);
+    postapi();
+  };
+
+  const getdata = (event) => {
+    setemail(event.target.value);
+  };
+
+  const resendotp = async () => {
+    axios
+      .post("https://eventformconatus.herokuapp.com/resendOTP", {
+        email: email,
+      })
+      .then(function (response) {
+        console.log(response);
+        url();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const store_otp = (event) => {
+    otp = otp.concat(event.target.value);
+    count = count + 1;
+    if (count === 4) {
+      count = 0;
+      url();
+    }
+    const { maxLength, value, name } = event.target;
+    const [fieldName, fieldIndex] = name.split("-");
+
+    var fieldIntIndex = parseInt(fieldIndex, 10);
+    if (value.length >= maxLength) {
+      if (fieldIntIndex < 4) {
+        nextfield = document.querySelector(
+          `input[name=field-${fieldIntIndex + 1}]`
+        );
       }
-
-       const showdiv = (e)=>{
-            e.preventDefault();
-            postapi();
-       };
-
-       const getdata=(event)=>{
-           setemail(event.target.value);
-       }
-
-       const url=()=>{
-        axios.post("https://event0form.herokuapp.com/verifyOTP/:id/",otp)
-        .then(function (response) {
-          console.log(response);
-          history.push("/form2");
-           history.go();
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-       }
-
-       const store_otp=(event)=>{
-           otp=otp.concat(event.target.value);
-           count=count+1;
-           if(count===4){
-               url();
-           }
-           const {maxLength, value, name}=event.target;
-           const [fieldName,fieldIndex]=name.split("-");
-
-           let fieldIntIndex=parseInt(fieldIndex,10);
-           if(value.length>=maxLength){
-             if(fieldIntIndex<4){
-               nextfield=document.querySelector(`input[name=field-${fieldIntIndex+1}]`);
-             }
-             if(nextfield!==null){
-               nextfield.focus();
-             }
-           }
-       }
-
-       useEffect(()=>{
-           console.log(email);
-       },[email]);
-
-  return(
-      <>
-       <div className="container">
-        <div className="screen" style={{display:show?"none":"block"}}>
-            <div className="screen__content">
+      if (nextfield !== null) {
+        nextfield.focus();
+      }
+    }
+  };
+  const abc = () => {
+    otp = "";
+    var i = 0;
+    while (i !== 4) {
+      i += 1;
+      var index = document.querySelector(`input[name=field-${i}]`);
+      index.value = "";
+    }
+    var foc = document.querySelector(`input[name=field-${1}]`);
+    foc.focus();
+  };
+  const url = () => {
+    axios
+      .post("https://eventformconatus.herokuapp.com/verifyOTP", {
+        email: email,
+        otp: otp,
+      })
+      .then(function (response) {
+        console.log(response);
+        history.push("/form2");
+        history.go();
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("Wrong OTP");
+        abc();
+      });
+  };
+  useEffect(() => {
+  }, [email]);
+  localStorage.setItem("email", email);
+  return (
+    <>
+      <div className="container">
+        <div className="screen" style={{ display: show ? "none" : "block" }}>
+          <div className="screen__content">
             <div>
-                <img src={blacklogo} alt="Logo" height="100" width="120"/>
+              <img src={blacklogo} alt="Logo" height="100" width="120" />
             </div>
-            <h1 className="heading">
-              Registration
-            </h1>
-                <form className="login">
-                    <div className="login__field">
-                        <input pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" type="text" className="login__input" placeholder="Enter Email" onChange={getdata}/>
-                    </div>
-                    <button className="login__submit button1" type="button" onClick={showdiv}>
-                         Generate OTP
-                    </button>
-                 </form>
-             </div>
-             <div className="screen__background">
-                 <span className="screen__background__shape screen__background__shape4"></span>
-                 <span className="screen__background__shape screen__background__shape3"></span>
-                 <span className="screen__background__shape screen__background__shape2"></span>
-                 <span className="screen__background__shape screen__background__shape1"></span>
-             </div>
-         </div>
-
-         <div className="screen" style={{display:show?"block":"none"}}>
-              <div className="screen__content">
-                <div>
-                  <img src={blacklogo} alt="Logo" height="100" width="120"/>
-                </div>
-                <h1 className="heading">
-                  Enter OTP
-                </h1>
-                <div className="otpbox">
-                  <input name="field-1" length="1" type="text" className="OTP" maxLength="1" required onChange={store_otp}/>
-                  <input name="field-2" length="1" type="text" className="OTP" maxLength="1" required onChange={store_otp}/>
-                  <input name="field-3" length="1" type="text" className="OTP" maxLength="1" required onChange={store_otp}/>
-                  <input name="field-4" length="1" type="text" className="OTP" maxLength="1" required onChange={store_otp}/>
-                </div>
-                  {/* <button className="login__submit button2" onClick={url}>
-                    <span className="button__text">Verify OTP</span>
-                  </button> */}
-                 <button className="login__submit button2 button4" type=''>
-                    <span className="button__text">Resend OTP</span>
-                 </button>
-                </div>
-               <div className="screen__background">
-                 <span className="screen__background__shape screen__background__shape4"></span>
-                 <span className="screen__background__shape screen__background__shape3"></span>
-                 <span className="screen__background__shape screen__background__shape2"></span>
-                 <span className="screen__background__shape screen__background__shape1"></span>
-               </div>
+            <h1 className="heading">Registration</h1>
+            <form className="login">
+              <div className="login__field">
+                <input
+                  pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+                  type="email"
+                  className="login__input"
+                  placeholder="Enter Email"
+                  onChange={getdata}
+                />
+              </div>
+              <button className="login__submit button1" onClick={showdiv}>
+                Generate OTP
+              </button>
+            </form>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4"></span>
+            <span className="screen__background__shape screen__background__shape3"></span>
+            <span className="screen__background__shape screen__background__shape2"></span>
+            <span className="screen__background__shape screen__background__shape1"></span>
+          </div>
         </div>
-     </div>
-     </>
+        <div className="screen" style={{ display: show ? "block" : "none" }}>
+          <div className="screen__content">
+            <div>
+              <img src={blacklogo} alt="Logo" height="100" width="120" />
+            </div>
+            <h1 className="heading">Enter OTP</h1>
+            <div className="otpbox">
+              <input
+                name="field-1"
+                length="1"
+                type="text"
+                className="OTP"
+                maxLength="1"
+                required
+                onChange={store_otp}
+              />
+              <input
+                name="field-2"
+                length="1"
+                type="text"
+                className="OTP"
+                maxLength="1"
+                required
+                onChange={store_otp}
+              />
+              <input
+                name="field-3"
+                length="1"
+                type="text"
+                className="OTP"
+                maxLength="1"
+                required
+                onChange={store_otp}
+              />
+              <input
+                name="field-4"
+                length="1"
+                type="text"
+                className="OTP"
+                maxLength="1"
+                required
+                onChange={store_otp}
+              />
+            </div>
+
+            <button
+              className="login__submit button2 button4"
+              onClick={resendotp}
+            >
+              <span className="button__text">Resend OTP</span>
+            </button>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4"></span>
+            <span className="screen__background__shape screen__background__shape3"></span>
+            <span className="screen__background__shape screen__background__shape2"></span>
+            <span className="screen__background__shape screen__background__shape1"></span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default Form;
